05da9e11651c58a52cf796e44cfd63fa80ffa84d
JeanPierreOdooController <jeanpierreluqueluna@gmail.com>
2026-07-14 13:20:39 -0500

    Update __manifest__.py

--- biocell_gestion_sedes_empresa_4/__manifest__.py
+++ biocell_gestion_sedes_empresa_4/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '15.0.1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['biocell_pe_facturacion_electronica_despatch', 'biocell_pe_facturacion_electronica_company_branch'],
+    'depends': ['biocell_pe_facturacion_electronica', 'biocell_gestion_sedes_empmresa_4'],
 
     # always loaded
     'data': [
